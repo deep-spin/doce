5a181848cffdd25761f12533e9892e6b518b8d0a
lhaausing <haausing.l@gmail.com>
2024-10-06 07:14:42 +0000

    updatecode for generating test cases

--- codegen/ut_gen.py
+++ codegen/ut_gen.py
@@ -5,6 +5,8 @@ from typing import List, Any
 
 import json
 import pickle
+import time
+import shutil
 
 from model import DecoderBase, make_model
 from rich.progress import (
@@ -16,9 +18,6 @@ from rich.progress import (
 )
 
 from vllm import SamplingParams
-
-##TODO:
-# 2. we should include the post-processing functions that process generated unit tests.
 
 def load_json(dir_path: str):
     with open(dir_path, "r") as f:
@@ -53,25 +52,57 @@ def ut_gen(
     gen_strs = [x.outputs[0].text.replace("\t", "    ") for x in vllm_outputs]
     return gen_strs
 
-def construct_ut_gen_prompt(prompt: str, entry_point: str) -> str:
-    return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
+#def construct_ut_gen_prompt(prompt: str, entry_point: str) -> str:
+#    return f"""Below is an instruction that describes a task. Write a response that appropriately completes the request.
 
 ### Instruction:
-I have this function stub, please generate 50 test cases for this function. The function stub is as follow:
-```python
-{prompt}
-```
-- Each test case is in the form of assertion statement, for example: assert {entry_point}(...) == ...
-- Each test case is in a single line
-- The length of each test case should be too long, ideally less than or equal to 150 letters
-- The test input should not be too long
-- The inputs of test cases should be diverse and cover corner cases of the function
-- Test cases should not be repeated
+#I have this function stub, please generate 50 test cases for this function. The function stub is as follow:
+#```python
+#{prompt}
+#```
+#- Each test case is in the form of assertion statement, for example: assert {entry_point}(...) == ...
+#- Each test case is in a single line
+#- The length of each test case should be too long, ideally less than or equal to 150 letters
+#- The test input should not be too long
+#- The inputs of test cases should be diverse and cover corner cases of the function
+#- Test cases should not be repeated
 
 ### Response:
-Here are 50 test cases for function `{entry_point}`:
-```python
-assert {entry_point}("""
+#Here are 50 test cases for function `{entry_point}`:
+#```python
+#assert {entry_point}("""
+
+def construct_ut_gen_prompt(prompt: str, entry_point: str) -> str:
+    return f"""# Given a docstring, continue to write the following code with 10 valid assertion statements to check the correctness of the function. Provide diverse test cases. 
+
+{prompt}
+# check the correctness of `{entry_point}` with 10 different valid assertion statements in the form of \"assert {entry_point}(...) == ...\"
+assert """
+
+def check_test_case_validation(test_case):
+    if len(test_case.strip()) < 1:
+        return False
+    if 'assert' not in test_case:
+        return False
+    try:
+        multi_line_test_case = test_case.replace("\n", "\n    ")
+        assert_in_a_block = f'try:\n    {multi_line_test_case}\nexcept:\n    pass\n'
+        compile(assert_in_a_block, '', 'exec')
+        return True
+    except Exception:
+        return False
+
+def test_case_extract(content, entry_point):
+    def _truncate(content):
+        for identifier in ['\nclass', '\ndef', '\n#', '\nif', '\nprint']:
+            if identifier in content:
+                content = content.split(identifier)[0]
+        return content.strip()
+    
+    split_by_assert = [f'assert {part}'.strip() for part in f'assert {content}'.split('assert ') if (entry_point.strip() in part) and len(part.strip()) > 0 and '==' in part]
+    truncated_test_cases = [_truncate(i) for i in split_by_assert]
+    checked_assertions = [i for i in truncated_test_cases if check_test_case_validation(i)]
+    return checked_assertions
 
 def code_generate(args, workdir: PathLike, model: DecoderBase, id_range=None):
     with Progress(
@@ -87,7 +118,7 @@ def code_generate(args, workdir: PathLike, model: DecoderBase, id_range=None):
             from evalplus.data import get_human_eval_plus
 
             dataset = get_human_eval_plus()
-            prompts = load_jsonl("/mnt/scratch-artemis/haausing/code_reranking/code/evalplus/other_data/HumanEval_for_test_case_generation.jsonl")
+            prompts = load_jsonl("other_data/HumanEval_for_test_case_generation_processed.jsonl")
             prompts = {prompt["task_id"]: prompt for prompt in prompts}
             assert all(task_id in dataset for task_id in prompts)
             
@@ -95,12 +126,12 @@ def code_generate(args, workdir: PathLike, model: DecoderBase, id_range=None):
             from evalplus.data import get_mbpp_plus
 
             dataset = get_mbpp_plus()
-            prompts = load_jsonl("/mnt/scratch-artemis/haausing/code_reranking/code/evalplus/other_data/mbpp_sanitized_for_test_case_generation.jsonl")
+            prompts = load_jsonl("other_data/mbpp_sanitized_for_test_case_generation_processed.jsonl")
             prompts = {prompt["task_id"].replace("MbppEval", "Mbpp"): prompt for prompt in prompts}
             assert all(task_id in prompts for task_id in dataset)
-
-        #final_logprobs = {} # this is a dictionary that will store the logprobs of the generated code
-
+        
+        
+        save_data = []
         for task_id, task in p.track(dataset.items()):
             if id_range is not None:
                 id_num = int(task_id.split("/")[1])
@@ -113,51 +144,49 @@ def code_generate(args, workdir: PathLike, model: DecoderBase, id_range=None):
             if args.contract_type != "none" and task["contract"] == "":
                 continue
 
-            os.makedirs(os.path.join(workdir, p_name), exist_ok=True)
             log = f"TestCaseGen: {p_name} @ {model}"
-            n_existing = 0
-            if args.resume:
-                # count existing .py files
-                n_existing = len(
-                    [
-                        f
-                        for f in os.listdir(os.path.join(workdir, p_name))
-                        if f.endswith(".py")
-                    ]
-                )
-                if n_existing > 0:
-                    log += f" (resuming from {n_existing})"
-
-            nsamples = args.n_samples - n_existing
             p.console.print(log)
-
-            sidx = args.n_samples - nsamples
             
             # I added the the replace method, just to make sure that the prompt is in the same format as the samples as the samples also include the prompt.
-            prompt = task["prompt"].replace("    ", "\t")
-            entry_point = task["entry_point"]
-            if dataset=="mbpp":
-                prompt = prompt.split("\n")[:-2]
-                prompt = "\n".join(prompt)
+            prompt = prompts[task_id]["prompt"].replace("    ", "\t")
+            entry_point = prompts[task_id]["entry_point"]
+            
             prompt = construct_ut_gen_prompt(prompt, entry_point)
 
-            while sidx < args.n_samples:
-                outputs = ut_gen(model, prompt, do_sample=not args.greedy, num_samples=args.n_samples - sidx)
-                assert outputs, "No outputs from model!"
-                for idx, impl in enumerate(outputs):
-                    try:
-                        with open(
-                            os.path.join(workdir, p_name, f"{sidx}.py"),
-                            "w",
-                            encoding="utf-8",
-                        ) as f:
-                            if model.conversational:
-                                f.write(impl)
-                            else:
-                                f.write(task["prompt"] + impl)
-                    except UnicodeEncodeError:
-                        continue
-                    sidx += 1
+            while True:
+                try:
+                    all_valid_test_cases = []
+                    for _ in range(16):
+                        rtn = ut_gen(model, prompt, do_sample=not args.greedy, num_samples=args.bs)
+                        assert len(rtn) >= 1
+                        for test_input_code in rtn:
+                            for single_assertion in test_case_extract(test_input_code, entry_point):
+                                if single_assertion not in all_valid_test_cases:
+                                    all_valid_test_cases.append(single_assertion)
+
+                        if len(all_valid_test_cases) >= 500:
+                            break
+                        time.sleep(10)
+
+                except Exception as e:
+                    print(e)
+
+                p.console.print(len(all_valid_test_cases))
+                ### check the generated test cases ###
+                if task_id.split("/")[-1] == "2":
+                    print("\n".join(all_valid_test_cases[:20]))
+                ### END of checking ###
+                save_data.append({"prompt": prompt, "test_cases": all_valid_test_cases, "task_id": task_id})
+
+                try:
+                    pickle.dump(save_data, open(f"{workdir}/test_cases_new.pkl", 'wb'))
+                    break
+                except Exception as e:
+                    print(e)
+                    save_data = save_data[:-1]
+                    continue
+
+            shutil.move(f"{workdir}/test_cases_new.pkl", f"{workdir}/test_cases.pkl")
 
 
 def main():
@@ -169,7 +198,6 @@ def main():
         "--dataset", required=True, type=str, choices=["humaneval", "mbpp"]
     )
     parser.add_argument("--root", type=str, required=True)
-    parser.add_argument("--n_samples", default=1, type=int)
     parser.add_argument("--resume", action="store_true")
     parser.add_argument(
         "--contract-type",
@@ -182,11 +210,10 @@ def main():
     parser.add_argument("--id-range", default=None, nargs="+", type=int)
     args = parser.parse_args()
 
-    if args.greedy and (args.temperature != 0 or args.bs != 1 or args.n_samples != 1):
+    if args.greedy and (args.temperature != 0 or args.bs != 1):
         args.temperature = 0
         args.bs = 1
-        args.n_samples = 1
-        print("Greedy decoding ON (--greedy): setting bs=1, n_samples=1, temperature=0")
+        print("Greedy decoding ON (--greedy): setting bs=1, temperature=0")
 
     if args.id_range is not None:
         assert len(args.id_range) == 2, "id_range must be a list of length 2"
