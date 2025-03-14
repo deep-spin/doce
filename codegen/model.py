3a4d50d3efc92994b734e63816f5daa2cd468f21
lhaausing <haausing.l@gmail.com>
2025-03-14 16:55:15 +0800

    update

--- codegen/model.py
+++ codegen/model.py
@@ -8,59 +8,11 @@ from warnings import warn
 os.environ["HF_HOME"] = os.environ.get("HF_HOME", "/path/to/huggingface")
 
 import openai
-import torch
-# from transformers import (
-#     AutoModelForCausalLM,
-#     AutoModelForSeq2SeqLM,
-#     AutoTokenizer,
-#     StoppingCriteria,
-#     StoppingCriteriaList,
-# )
 from vllm import LLM, SamplingParams
 
 from evalplus.gen.util.api_request import make_auto_request
 
 EOS = ["<|endoftext|>", "<|endofmask|>", "</s>"]
-
-
-# Adopted from https://github.com/huggingface/transformers/pull/14897
-# class EndOfFunctionCriteria(StoppingCriteria):
-#     def __init__(self, start_length, eos, tokenizer, *args, **kwargs):
-#         super().__init__(*args, **kwargs)
-#         self.start_length = start_length
-#         self.eos = eos
-#         self.tokenizer = tokenizer
-#         self.end_length = {}
-
-#     def __call__(self, input_ids, scores, **kwargs):
-#         """Returns true if all generated sequences contain any of the end-of-function strings."""
-#         decoded_generations = self.tokenizer.batch_decode(
-#             input_ids[:, self.start_length :]
-#         )
-#         done = []
-#         for index, decoded_generation in enumerate(decoded_generations):
-#             finished = any(
-#                 [stop_string in decoded_generation for stop_string in self.eos]
-#             )
-#             if (
-#                 finished and index not in self.end_length
-#             ):  # ensures first time we see it
-#                 for stop_string in self.eos:
-#                     if stop_string in decoded_generation:
-#                         self.end_length[index] = len(
-#                             input_ids[
-#                                 index,  # get length of actual generation
-#                                 self.start_length : -len(
-#                                     self.tokenizer.encode(
-#                                         stop_string,
-#                                         add_special_tokens=False,
-#                                         return_tensors="pt",
-#                                     )[0]
-#                                 ),
-#                             ]
-#                         )
-#             done.append(finished)
-#         return all(done)
 
 
 class DecoderBase(ABC):
